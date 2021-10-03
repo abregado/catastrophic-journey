2b54d7402ed091f84b814e1b472c6e55d3f95842
Ben Buckton <contact@benbuckton.com>
2021-10-03 19:17:56 +0200

    WIP Build

--- Assets/Scripts/EventMarker.cs
+++ Assets/Scripts/EventMarker.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class EventMarker : MonoBehaviour {
--- Assets/Scripts/Res.cs
+++ Assets/Scripts/Res.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(fileName = "Res", menuName = "ScriptableObjects/Res", order = 1)]
 public class Res : ScriptableObject
